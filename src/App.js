9da850ab9c79fe5ce6d9fb6c3f51ee37461cf3fb
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-10-06 21:57:17 +0600

    Part 23 - getByPlaceholderText and getAllByPlaceholderText

--- src/App.js
+++ src/App.js
@@ -4,25 +4,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>RTL Query : getByLabelText</h1>
+      <h1>RTL Query : ByPlaceholderText</h1>
+      <input type="text" placeholder='enter username' />
+      <input type="text" placeholder='enter username' />
 
-      <label htmlFor="user-email1">UserName</label>
-      <input type="text" id="user-email1" defaultValue={'khalid'} />
-
-      <label htmlFor="user-email2">UserName</label>
-      <input type="text" id="user-email2" defaultValue={'khalid'} />
-
-      <label htmlFor="user-email3">UserName</label>
-      <input type="text" id="user-email3" defaultValue={'khalid'} />
-
-      <label htmlFor="skill">Skills</label>
-      <input type="checkbox" id="skill" defaultChecked={true}/>
-      
-      <label htmlFor="skill2">Skills</label>
-      <input type="checkbox" id="skill2" defaultChecked={true}/>
-
-      <label htmlFor="skill3">Skills</label>
-      <input type="checkbox" id="skill3" defaultChecked={true}/>
 
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -2,27 +2,20 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 
-test('input test case', () => {
-  render(<App />);
+// test('test case for single input field', () => {
+//   render(<App />);
+//   const input = screen.getByPlaceholderText('enter username');
 
-  const inputs = screen.getAllByLabelText('UserName');
-  // expect(inputs[0]).toBeInTheDocument();
+//   expect(input).toBeInTheDocument();
+// });
+
+
+test('test case for multiple input fields', () => {
+  render(<App />);
+  const inputs = screen.getAllByPlaceholderText('enter username');
 
   for (let i = 0; i < inputs.length; i++){
     expect(inputs[i]).toBeInTheDocument();
-    expect(inputs[i]).toHaveValue('khalid');
   }
-});
 
-
-test('checkbox test case', () => {
-  render(<App />);
-
-  const checkboxs = screen.getAllByLabelText('Skills');
-  // expect(inputs[0]).toBeInTheDocument();
-
-  for (let i = 0; i < checkboxs.length; i++) {
-    expect(checkboxs[i]).toBeInTheDocument();
-    expect(checkboxs[i]).toBeChecked();
-  }
-});
+})
