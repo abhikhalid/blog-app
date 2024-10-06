57ebee23c7a41187cadac984c9e2b81c6fece50d
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-10-06 12:59:50 +0600

    React Testing Library and JEST in Hindi #3 Write Basic Test Case

---
+++ src/sum.test.js
@@ -0,0 +1,18 @@
+import sum from "./sum";
+
+test("testing for sum function", () => {
+    let a = 10;
+    let b = 20;
+    let output = a + b;
+
+    expect(sum(a,b)).toBe(output);   
+});
+
+
+test("testing for sum function 2", () => {
+    let a = 100;
+    let b = 200;
+    let output = a + b;
+
+    expect(sum(a, b)).toBe(output);
+});
