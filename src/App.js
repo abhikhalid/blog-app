590421fcf56e5935d943d1f1e329671278ac52e6
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-10-06 19:05:26 +0600

    Part 16: Functional Component method testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 import './App.css';
+import handleOtherMethod from './helper';
 
 function App() {
   const [data, setData] = useState("");
 
+  const handleData = () => {
+    setData('hello');
+  }
+  
+  // const handleOtherMethod = () => {
+  //   console.log('hi');
+  //   return "hi";
+  // }
 
   return (
     <div className="App">
-      <h1>Test Click Event with Button</h1>
-
-      <button onClick={() => setData('updated data')}>Update Data</button>
-      
-      <h1>{data}</h1>
-
+      <h2>Functional Component method testing</h2>
+      <button data-testid="btn1" onClick={handleData}>Update</button>
+      <button onClick={handleOtherMethod}>Print</button>
+      <h2>{data}</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,55 +1,17 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
-import cleandb from './service';
-
-// beforeAll(() => {
-//   console.log(`***** before all hook *****`);
-// });
+import handleOtherMethod from './helper';
 
 
-beforeEach(() => {
-  // console.log(`***** before each hook *****`);
-  cleandb();
-});
-
-
-test('click event test case 1', () => {
-  console.log("1");
-
+test('method testing case 1', () => {
   render(<App />);
 
-  const btn = screen.getByRole("button");
+  const btn = screen.getByTestId('btn1');
   fireEvent.click(btn);
-
-  expect(screen.getByText('updated data')).toBeInTheDocument();
+  expect(screen.getByText('hello')).toBeInTheDocument();
 });
 
-test('click event test case 2', () => {
-  console.log("2");
 
-  render(<App />);
-
-  const btn = screen.getByRole("button");
-  fireEvent.click(btn);
-
-  expect(screen.getByText('updated data')).toBeInTheDocument();
-});
-
-test('click event test case 3', () => {
-  console.log("3");
-
-  render(<App />);
-
-  const btn = screen.getByRole("button");
-  fireEvent.click(btn);
-
-  expect(screen.getByText('updated data')).toBeInTheDocument();
-});
-
-// afterAll(() => {
-//   console.log("____________ after all ________");
-// })
-
-afterEach(() => {
-  console.log(`______ after each _____`);
+test("method testing case 2", () => {
+  expect(handleOtherMethod()).toMatch('hi');
 });
