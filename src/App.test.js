b258bbaf76eb62213f0de3c11eda81ea76ac0362
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-10-06 15:59:16 +0600

    Part 12: beforeEach | beforeAll | afterAll | afterEach hooks

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,55 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import cleandb from './service';
 
-test('click event test case', () => {
+// beforeAll(() => {
+//   console.log(`***** before all hook *****`);
+// });
+
+
+beforeEach(() => {
+  // console.log(`***** before each hook *****`);
+  cleandb();
+});
+
+
+test('click event test case 1', () => {
+  console.log("1");
+
   render(<App />);
 
   const btn = screen.getByRole("button");
   fireEvent.click(btn);
 
   expect(screen.getByText('updated data')).toBeInTheDocument();
+});
+
+test('click event test case 2', () => {
+  console.log("2");
+
+  render(<App />);
+
+  const btn = screen.getByRole("button");
+  fireEvent.click(btn);
+
+  expect(screen.getByText('updated data')).toBeInTheDocument();
+});
+
+test('click event test case 3', () => {
+  console.log("3");
+
+  render(<App />);
+
+  const btn = screen.getByRole("button");
+  fireEvent.click(btn);
+
+  expect(screen.getByText('updated data')).toBeInTheDocument();
+});
+
+// afterAll(() => {
+//   console.log("____________ after all ________");
+// })
+
+afterEach(() => {
+  console.log(`______ after each _____`);
 });
